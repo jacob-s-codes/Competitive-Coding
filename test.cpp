ea5bc4043088c1f2c91465e7731bc7dceb7b6735
jacob-s-codes <jacobbshaul@gmail.com>
2025-02-12 19:06:34 -0800

    initial commit

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long
+
+// use long long for whenever you're using an int
+// regardless.
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    cout<<"Hello world";
+
+    return 0;
+}
