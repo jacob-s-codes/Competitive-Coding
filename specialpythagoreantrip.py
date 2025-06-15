fa0b6b467ceaa0fd11b2feebeaaa6f30c22337d2
jacob-s-codes <jacobbshaul@gmail.com>
2025-06-14 22:12:54 -0700

    solved special pythagorean triple

--- specialpythagoreantrip.py
+++ specialpythagoreantrip.py
@@ -1,3 +1,8 @@
-a = 10
-b = 10
-c = 10
+def findtrip():
+    for a in range(1, 1000):
+        for b in range(a+1, 1000):
+            if a**2 + b**2 == (1000-a-b)**2:
+                return [a, b]
+    return 0
+
+print(findtrip())
